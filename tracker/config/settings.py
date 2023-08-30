b57b929e90224006df4c5d0f12da8e882a8f82ce
Masum Billal <proafxin@users.noreply.github.com>
2023-08-31 05:52:35 +0600

    db config

--- tracker/config/settings.py
+++ tracker/config/settings.py
@@ -2,3 +2,6 @@ from os import environ
 
 DBNAME = environ["MYSQL_DBNAME"]
 DBUSER = environ["MYSQL_USER"]
+DBPASSWORD = environ["MYSQL_PASSWORD"]
+DBHOST = environ["MYSQL_HOST"]
+DBPORT = environ["MYSQL_PORT"]
