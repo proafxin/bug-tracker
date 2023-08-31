80cb3267174b0ad589efe5b1d318231a8d0f722e
Masum Billal <proafxin@users.noreply.github.com>
2023-08-31 06:14:09 +0600

    fixed import order

--- tracker/db/session.py
+++ tracker/db/session.py
@@ -1,10 +1,8 @@
-from tracker.config.settings import DBUSER, DBPASSWORD, DBHOST, DBPORT, DBNAME
-
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from sqlalchemy.ext.declarative import declarative_base
-
+from tracker.config.settings import DBHOST, DBNAME, DBPASSWORD, DBPORT, DBUSER
 
 url = f"mysql+pymysql://{DBUSER}:{DBPASSWORD}@{DBHOST}:{DBPORT}/{DBNAME}"
 engine = create_engine(url=url)
